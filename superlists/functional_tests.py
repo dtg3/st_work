43ca4f52d4fa2e964beb51865334d9ed37f51a35
Drew Guarnera <dguarner@kent.edu>
2015-09-20 15:51:19 -0400

    Update functional test to be a test class

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,40 +1,47 @@
 from selenium import webdriver
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
 
-browser = webdriver.Firefox()
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-# The Doctor's Tardis is acting up. He's decided to use an
-# 	online to-do app to keep track of all the things he needs
-# 	to make the necesary repairs
+	def tearDown(self):
+		self.browser.quit()
 
-# He visits the website for the app
-browser.get('http://localhost:8000')
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# The Doctor's Tardis is acting up. He's decided to use an
+		# 	online to-do app to keep track of all the things he needs
+		# 	to make the necesary repairs
 
-# He notices the the application title mentions to-do
-assert 'To-Do' in browser.title
+		# He visits the website for the app
+		self.browser.get('http://localhost:8000')
 
-# The starting screen allows The Doctor it jump right in and make
-#	a new to-do item
+		# He notices the the application title mentions to-do
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
 
-# He enters in "Replace space-time locator"
+		# The starting screen allows The Doctor it jump right in and make
+		#	a new to-do item
 
-# He hits enter, and the page updates showing:
-# 	"1: Replace space-time locator"
+		# He enters in "Replace space-time locator"
 
-# An empty text box remains on the page allowing for another to-do item
-# 	to be entered. The Doctor enters:
-#	"Repair Tardis console"
+		# He hits enter, and the page updates showing:
+		# 	"1: Replace space-time locator"
 
-# The page updates again, and now shows both items on the list
+		# An empty text box remains on the page allowing for another to-do item
+		# 	to be entered. The Doctor enters:
+		#	"Repair Tardis console"
 
-# The Doctor is always busy saving the planet (or any planet), so he needs
-# 	to make sure that the site lets him save his list for later.
-#	Upon saving the list, a unique URL is generated for him along with some
-# 	explanatory text to that effect
+		# The page updates again, and now shows both items on the list
 
-# The Doctor visits the URL and makes sure that the list appears
+		# The Doctor is always busy saving the planet (or any planet), so he needs
+		# 	to make sure that the site lets him save his list for later.
+		#	Upon saving the list, a unique URL is generated for him along with some
+		# 	explanatory text to that effect
 
-# With the Tardis back up and running, The Doctor continues
-#	his travels.
+		# The Doctor visits the URL and makes sure that the list appears
 
-browser.quit()
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
