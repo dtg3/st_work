bbac8c40b9d7ac06bb781b916e7f40ee38e9c723
Drew Guarnera <dguarner@kent.edu>
2015-09-25 16:54:42 -0400

    Fix issues with the functional tests with template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -41,7 +41,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Replace space-time locator' for row in rows)
+			any(row.text == '1: Replace space-time locator' for row in rows),
+			"New to-do item did not appear in table"
 		)
 
 		# An empty text box remains on the page allowing for another to-do item
