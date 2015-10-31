d7567b9b9aa1860a7c4c962cfecb102f618b03ad
Heather Michaud <hmm34@zips.uakron.edu>
2015-10-31 18:17:04 -0400

    Add view ISBN test

--- hw8/nypl/nypl_acceptance.py
+++ hw8/nypl/nypl_acceptance.py
@@ -1,10 +1,13 @@
 import unittest
-import nypl_search, nypl_responsive
+import nypl_search, nypl_responsive, nypl_view
 
 class Test_000_Search(nypl_search.NYPLSearch):
 	pass
 
-class Test_001_Responsive(nypl_responsive.NYPLResponsive):
+class Test_001_View(nypl_view.NYPLView):
+	pass
+
+class Test_002_Responsive(nypl_responsive.NYPLResponsive):
 	pass
 
 if __name__ == "__main__":
---
+++ hw8/nypl/nypl_view.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class NYPLView(unittest.TestCase):
+    nypl_new_catalog = "http://browse.nypl.org"
+    nypl_old_catalog = "http://catalog.nypl.org"
+
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors","test-type"])
+        options.add_argument("--start-maximized")
+        options.add_argument('--disable-application-cache')
+        self.browser = webdriver.Chrome(chrome_options=options)
+        self.browser.get(self.nypl_new_catalog)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def t_can_get_isbn(self, isbn):
+        search_bar = self.browser.find_element_by_id("searchString")
+        search_bar.send_keys(isbn)
+        search_bar.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(5)
+
+         # we get one results
+        titleDivs = self.browser.find_elements_by_class_name("dpBibTitle")
+        self.assertTrue(len(titleDivs) > 0, "Could not retrieve any results based on isbn " + isbn)
+
+        # result matches our expected title
+        titleDivs[0].find_element_by_xpath("./span/a").click()
+        self.browser.implicitly_wait(3)
+
+        # that result has the ISBN we searched for
+        additionalInfoDivs = self.browser.find_elements_by_class_name("bibInfoData")
+        addInfo = []
+        for item in additionalInfoDivs:
+            addInfo.append(item.text)
+
+        # we can view the isbn
+        foundISBN = False
+        for item in additionalInfoDivs:
+            if isbn in item.text:
+                foundISBN = True
+        self.assertTrue(foundISBN, "Could not retrieve ISBN " + isbn + ".")
+
+    def test_001_view_isbn(self):
+        self.t_can_get_isbn("0345391802")
+        self.t_can_get_isbn("1451673310")
+        self.t_can_get_isbn("0471043281")
+        self.t_can_get_isbn("0811874559")
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
