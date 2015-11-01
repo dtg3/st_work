1ee14d85cfdf2de96cf0ba882d0af38da34232f4
Heather Michaud <hmm34@zips.uakron.edu>
2015-10-31 22:48:13 -0400

    Add non-ascii search test

--- hw8/nypl/nypl_secure.py
+++ hw8/nypl/nypl_secure.py
@@ -5,14 +5,27 @@ import unittest
 import nypl_utility as utils
 
 class NYPLSecure(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.get(utils.nypl_new_catalog)
-        self.browser.implicitly_wait(3)
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.get(utils.nypl_new_catalog)
+		self.browser.implicitly_wait(3)
 
-    def tearDown(self):
-        self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_search_with_nonascii_characters(self):
+		# look for the art of war
+		utils.search_new_catalog_by_keyword(self.browser, "孫子兵法")
+		titleDivs = self.browser.find_elements_by_class_name("dpBibTitle")
+		self.assertTrue(len(titleDivs) > 0,
+            "Did not return any results when searching for the art of war in chinese")
+
+		# look for crime and punishment
+		utils.search_new_catalog_by_keyword(self.browser, "Преступление и наказание")
+		titleDivs = self.browser.find_elements_by_class_name("dpBibTitle")
+		self.assertTrue(len(titleDivs) > 0,
+            "Did not return any results when searching for crime and punishment in russian")
 
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+	unittest.main(warnings='ignore')
