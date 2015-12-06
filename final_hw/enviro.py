fa0f79af581a0952e0f86e6ee5f0d177966026f4
Drew Guarnera <dguarner@kent.edu>
2015-12-05 22:53:58 -0500

    Add test for python3 and pip3

--- final_hw/enviro.py
+++ final_hw/enviro.py
@@ -3,7 +3,7 @@ from private import host, username, password
 
 import unittest
 
-class Test_000_Utils(unittest.TestCase):
+class Test_000_Environment(unittest.TestCase):
     def setUp(self):
         env.host_string = host
         env.user = username
@@ -11,6 +11,12 @@ class Test_000_Utils(unittest.TestCase):
 
     def test_000_tmux(self):
         self.assertTrue(run("which tmux"))
+
+    def test_001_python3(self):
+        self.assertTrue(run("which python3"))
+
+    def test_002_pip3(self):
+        self.assertTrue(run("which pip3"))
 
 if __name__ == "__main__":
     unittest.main()
