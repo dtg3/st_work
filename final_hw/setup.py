3e49ba5a500f29c96c965f536576e4c578589349
Drew Guarnera <dguarner@kent.edu>
2015-12-05 22:49:06 -0500

    Refactor tmux installation

--- final_hw/setup.py
+++ final_hw/setup.py
@@ -3,18 +3,19 @@ from private import host, username, password, sudo
 
 import unittest
 
-
+def install(appName):
+    result = run( "echo \'" + sudo + "\' | sudo -kS apt-get -q -y --force-yes install " + appName + " 2>&1")
+    for line in result.lower().splitlines():
+        if (line.startswith("e:")):
+            print(result)
+            exit()
+    print ("Installed: " + appName)
 
 def install_utils():
     # install tmux
     if (not run("which tmux")):
-        result = run( "echo \'" + sudo + "\' | sudo -kS apt-get -q -y --force-yes install tmux 2>&1")
-        for line in result.lower().splitlines():
-            if (line.startswith("e:")):
-                print(result)
-                exit()
-        print ("Installed: tmux")
-
+        install("tmux")
+        
 
 if __name__ == "__main__":
     env.host_string = host
