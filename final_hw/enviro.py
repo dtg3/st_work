072e67d7430c3b0144ff1a6bd7dde73cc699b7cd
Drew Guarnera <dguarner@kent.edu>
2015-12-07 07:54:18 -0500

    Add tests for App install and app install setup

--- final_hw/enviro.py
+++ final_hw/enviro.py
@@ -26,6 +26,28 @@ class Test_000_Environment(unittest.TestCase):
     def test_004_wget(self):
         self.assertTrue(run("which wget"))
 
+class Test_001_AppInstall(unittest.TestCase):
+    def setUp(self):
+        env.host_string = host
+        env.user = username
+        env.password = password
 
+    def test_000_app_directories(self):
+        self.assertTrue(run("ls -l | grep webapps"))
+        self.assertTrue(run("cd webapps; ls -l | grep todo"))
+        self.assertTrue(run("cd webapps/todo; ls -l | grep superlists"))
+        self.assertTrue(run("cd webapps/todo/; ls -l | grep database"))
+        self.assertTrue(run("cd webapps/todo/database; ls -l | grep db.sqlite3"))
+'''
+class Test_002_LaunchApp(unittest.TestCase):
+    def setUp(self):
+        env.host_string = host
+        env.user = username
+        env.password = password
+
+    def test_000_launch_app(self):
+        results = run("tmux list-sessions 2>&1")        
+        self.assertFalse("failed to connect to server: Connection refused" in results)
+'''
 if __name__ == "__main__":
     unittest.main()
--- final_hw/setup.py
+++ final_hw/setup.py
@@ -11,27 +11,58 @@ def install(appName):
             exit()
     print ("Installed: " + appName)
 
-def install_utils():
+def install_reqs():
     # install tmux
     if (not run("which tmux")):
         install("tmux")
 
+    # install pyhton3
     if (not run("which python3")):
         install("python3")
 
+    # install pip3
     if (not run("which pip3")):
         install("python3-pip")
 
+    # install django 1.8
     if (not run("pip3 list | grep -io django")):
         result = run( "echo \'" + sudo + "\' | sudo -kS pip3 install django==1.8.4")
         print("Installed: Django")
 
+    # install wget
     if (not run("which wget")):
         install("wget")
+
+def app_setup():
+    # download app and make root directory
+    if (not run("ls -l | grep webapps")):
+        run("wget https://github.com/dtg3/st-site/archive/master.zip -O webapp.zip 2>&1")
+        run("mkdir webapps")
+
+    # unzip app and cleanup zip file
+    if (not run("ls -l webapps | grep todo")):
+        run("unzip webapp.zip -d webapps/todo")
+        run ("rm webapp.zip")
+    
+    # rename application directory
+    if (not run("cd webapps/todo; ls -l | grep superlists")):
+        run("cd webapps/todo; mv st-site-master superlists")
+
+    # create database directory
+    if (not run("cd webapps/todo ls -l | grep database")):
+        run("cd webapps/todo; mkdir database")
+    
+    # run databse migration
+    if (not run("cd webapps/todo/database; ls -l | grep db.sqlite3")):
+        run("cd webapps/todo/superlists; python3 manage.py migrate --noinput")
+        #run("cd webapps/todo/superlists; python manage.py runserver 0.0.0.0:8000")
 
 if __name__ == "__main__":
     env.host_string = host
     env.user = username
     env.password = password
 
-    install_utils()
+    print("Install Requirements")
+    install_reqs()
+    print("Install App")
+    app_setup()
